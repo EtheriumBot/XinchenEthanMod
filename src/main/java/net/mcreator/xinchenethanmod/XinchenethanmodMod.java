fb0838dd866df0ace85e9fed7ab102500cb0bba5
Ethan Wu <ethanrwu@gmail.com>
2025-07-06 14:54:22 -0700

    Added MCreator Workspace

---
+++ src/main/java/net/mcreator/xinchenethanmod/XinchenethanmodMod.java
@@ -0,0 +1,17 @@
+package net.mcreator.xinchenethanmod;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import net.fabricmc.api.ModInitializer;
+
+public class XinchenethanmodMod implements ModInitializer {
+	public static final String MOD_ID = "xinchenethanmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Starting XinchenethanmodMod");
+
+	}
+}
---
+++ src/main/java/net/mcreator/xinchenethanmod/XinchenethanmodModClient.java
@@ -0,0 +1,12 @@
+package net.mcreator.xinchenethanmod;
+
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ClientModInitializer;
+
+@Environment(EnvType.CLIENT)
+public class XinchenethanmodModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
